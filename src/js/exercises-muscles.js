a0e4ec884f916cfbc455bc4f87dbc170c2eeb9b8
Dzu-te <108475129+Dzu-te@users.noreply.github.com>
2024-02-07 17:31:11 +0200

    add css/muscles-section
    & delte <a> from buttons

---
+++ src/js/exercises-muscles.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'https://energyflow.b.goit.study/api';
+
+const containerMuscles = document.querySelector('container-muscles');
+const exercisesContainer = document.querySelector('exercises-container');
