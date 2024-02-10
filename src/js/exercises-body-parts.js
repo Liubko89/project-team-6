fc6442210e7020da1eb7a7814610c9e0fe077a16
NatalyMit <mitenkova91@icloud.com>
2024-02-10 17:06:59 +0100

    add collection

--- src/js/exercises-body-parts.js
+++ src/js/exercises-body-parts.js
@@ -1,8 +1,15 @@
 const containerMuscles = document.querySelector('.exercises-nav-list');
+const hiddenContainer = document.querySelector('.container-muscles');
 const exGroupCardList = document.querySelector('.exercises-container');
 const imageEl = document.querySelector('.exercise-card');
 const listExercisesEl = document.querySelector('.exercises-gallery');
-const breadcrumb = document.querySelector('.exercise-group');
+const breadcrumb = document.querySelector('.header-box');
+const filterBtn = document.querySelector('.exercises-search');
+const exercisesFilterSection = document.querySelector(
+  '.exercise-filters-section'
+);
+
+// let currentGroup = 'Waist';
 
 const BASE_URL = `https://energyflow.b.goit.study/api/`;
 const filterDict = {
@@ -13,18 +20,30 @@ const filterDict = {
 const hiddenClass = 'is-hidden';
 
 exGroupCardList.addEventListener('click', handleGroupSelection);
-exGroupCardList.classList.remove(hiddenClass);
+hiddenContainer.classList.remove(hiddenClass);
+exercisesFilterSection.classList.add(hiddenClass);
+
 // getExercises('Muscles').then(renderExerciseCards);
 
 async function handleGroupSelection(evt) {
   const card = evt.target.closest('.exercise-card');
 
   if (!card) return;
-
+  // currentGroup = { group };
   const { filter, group } = card.dataset;
 
   await getExercises(filterDict[filter], group).then(renderExerciseCards);
-  exGroupCardList.classList.add(hiddenClass);
+  hiddenContainer.classList.add(hiddenClass);
+  exercisesFilterSection.classList.remove(hiddenClass);
+  filterBtn.addEventListener('click', handleBackClick);
+
+  function handleBackClick(evt) {
+    console.log(evt.target);
+    if (currentTarget === evt.target) {
+      hiddenContainer.classList.remove(hiddenClass);
+      exercisesFilterSection.classList.add(hiddenClass);
+    }
+  }
 }
 
 async function getExercises(filter, group) {
@@ -45,14 +64,14 @@ function createExerciseCard({
   <div class="box-up">
   <div class="box-left">
   <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
-  <div class="rating-stars"><p class="rating-par">${rating}</p><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
+  <div class="rating-stars"><p class="rating-par">${rating}</p><svg class="icon-star" width="18" height="18"><use href="./svg/icons.svg#icon-star"></use></svg></div></div>
 
   
-  <button type = "button" class="btn-start-arrow">START<svg class="icon-arrow" width="14" height="14"><use href="../svg/icons.svg#icon-arrow"></use></svg></button>
+  <button type = "button" class="btn-start-arrow">START<svg class="icon-arrow" width="14" height="14"><use href="./svg/icons.svg#icon-arrow"></use></svg></button>
   </div>
            
             <div class="exercises-par"> 
-            <div class="options-item-span"><svg class="icon-men" width="18" height="18"><use href="../svg/icons.svg#icon-running-man"></use></svg></div>
+            <div class="options-item-span"><svg class="icon-men" width="18" height="18"><use href="./svg/icons.svg#icon-running-man"></use></svg></div>
             <h4 class="ex-name">${name}</h4>
             </div>
            
@@ -67,3 +86,14 @@ function createExerciseCard({
 function renderExerciseCards(exercises) {
   listExercisesEl.innerHTML = exercises.map(createExerciseCard).join('');
 }
+// function renderExerciseContainer({ results }) {
+//   renderExerciseCards(results);
+//   renderHeaderGroup(group);
+// }
+
+// function renderHeaderGroup(group) {
+//   breadcrumb.insertAdjacentHTML(
+//     'beforeend',
+//     `<h3 class="exercise-group">${group}</h3>`
+//   );
+// }
