16ace607cba5cbec5a61d67fe626b531045c6eb6
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-10 22:41:44 +0000

    Deploying to gh-pages from @ Liubko89/project-team-6@150f09f0889bdf8a238e71e2ef0ff897965479c4 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,5 +1,5 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./assets/vendor-8cce9181.js";l.defaults.baseURL="https://energyflow.b.goit.study/api";const B=document.querySelector(".js-close-menu"),T=document.querySelector(".js-open-menu"),x=document.querySelector(".js-menu-container");B.addEventListener("click",C);T.addEventListener("click",D);function C(){x.classList.add("is-hidden")}function D(){x.classList.remove("is-hidden")}const R="https://energyflow.b.goit.study/api",f=document.querySelector(".quote__backend");async function H(){f.innerHTML='<span class="loader"></span>';try{return(await l.get(`${R}/quote`)).data}catch(e){throw c.error({title:"Wrong",message:"Try again",position:"topRight"}),e}}function I(e,t){return`<p class="quote__backend-text">${t}</p>
-    <p class="quote__backend-author">${e}</p>`}async function _(){try{const{author:e,quote:t}=await H(),s={author:e,quote:t,savedDate:new Date().toLocaleDateString()};localStorage.setItem("quote",JSON.stringify(s)),localStorage.setItem("savedDate",s.savedDate),h(e,t)}catch(e){console.error(e)}}function h(e,t){f.innerHTML=I(e,t)}async function y(){const e=JSON.parse(localStorage.getItem("quote")),t=localStorage.getItem("savedDate"),s=new Date().toLocaleDateString();e&&t===s?h(e.author,e.quote):await _()}y();function b(){const e=new Date,t=new Date(e.getFullYear(),e.getMonth(),e.getDate()+1,0,0,0,0)-e;setTimeout(function(){y(),b()},t)}b();const $=document.querySelector(".exercises-nav-list"),j=document.querySelector(".exercises-container"),o=document.querySelector(".pagination"),U="https://energyflow.b.goit.study/api";let m="Muscles";$.addEventListener("click",A);o.addEventListener("click",N);g(m).then(v);async function A(e){const{filter:t}=e.target.dataset;if(!t)return;m=t,await g(t).then(v);const s=$.querySelector(".active"),a=e.target;s.disabled=!1,a.disabled=!0,s.classList.remove("active"),a.classList.add("active")}async function g(e,t=1){return fetch(`${U}/filters?filter=${e}&page=${t}&limit=12`).then(s=>s.json())}function v({results:e,page:t,totalPages:s}){Q(e),O(s,t)}function Q(e){j.innerHTML=e.map(P).join("")}function P({name:e,filter:t,imgUrl:s}){return`
+import"./assets/header-hero-27cb5b9d.js";import{a as l,i as n}from"./assets/vendor-8cce9181.js";l.defaults.baseURL="https://energyflow.b.goit.study/api";const k="https://energyflow.b.goit.study/api",x=document.querySelector(".quote__backend");async function B(){x.innerHTML='<span class="loader"></span>';try{return(await l.get(`${k}/quote`)).data}catch(e){throw n.error({title:"Wrong",message:"Try again",position:"topRight"}),e}}function C(e,t){return`<p class="quote__backend-text">${t}</p>
+    <p class="quote__backend-author">${e}</p>`}async function D(){try{const{author:e,quote:t}=await B(),s={author:e,quote:t,savedDate:new Date().toLocaleDateString()};localStorage.setItem("quote",JSON.stringify(s)),localStorage.setItem("savedDate",s.savedDate),f(e,t)}catch(e){console.error(e)}}function f(e,t){x.innerHTML=C(e,t)}async function h(){const e=JSON.parse(localStorage.getItem("quote")),t=localStorage.getItem("savedDate"),s=new Date().toLocaleDateString();e&&t===s?f(e.author,e.quote):await D()}h();function y(){const e=new Date,t=new Date(e.getFullYear(),e.getMonth(),e.getDate()+1,0,0,0,0)-e;setTimeout(function(){h(),y()},t)}y();const b=document.querySelector(".exercises-nav-list"),M=document.querySelector(".exercises-container"),o=document.querySelector(".pagination"),R="https://energyflow.b.goit.study/api";let g="Muscles";b.addEventListener("click",I);o.addEventListener("click",Q);m(g).then(v);async function I(e){const{filter:t}=e.target.dataset;if(!t)return;g=t,await m(t).then(v);const s=b.querySelector(".active"),a=e.target;s.disabled=!1,a.disabled=!0,s.classList.remove("active"),a.classList.add("active")}async function m(e,t=1){return fetch(`${R}/filters?filter=${e}&page=${t}&limit=12`).then(s=>s.json())}function v({results:e,page:t,totalPages:s}){_(e),A(s,t)}function _(e){M.innerHTML=e.map(U).join("")}function U({name:e,filter:t,imgUrl:s}){return`
     <li class="exercise-card" data-filter="${t}" data-group="${e}">
       <div class="exercise-card-img">
         <img class="exercise-card-img" src="${s}" alt="${e}">
@@ -9,11 +9,11 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./
         </div>      
       </div>
     </li>
-  `}function O(e,t){if(e<=1){o.innerHTML="";return}o.innerHTML=Array(e).fill(1).map((a,i)=>a+i).map(F).join("");const s=o.children[t-1];s.classList.add("active"),s.firstElementChild.disabled=!0}function F(e){return`
+  `}function A(e,t){if(e<=1){o.innerHTML="";return}o.innerHTML=Array(e).fill(1).map((a,i)=>a+i).map(H).join("");const s=o.children[t-1];s.classList.add("active"),s.firstElementChild.disabled=!0}function H(e){return`
     <li class="page" data-page="${e}">
       <button>${e}</button>
     </li>
-  `}function N(e){var s;const t=(s=e.target.closest(".page"))==null?void 0:s.dataset.page;t&&g(m,t).then(v)}document.querySelector(".exercises-nav-list");const u=document.querySelector(".exercises"),G=document.querySelector(".exercises-container");document.querySelector(".exercise-card");const W=document.querySelector(".exercises-gallery");document.querySelector(".header-box");const J=document.querySelector(".exercises-search"),p=document.querySelector(".exercise-filters-section"),z="https://energyflow.b.goit.study/api/",K={Muscles:"muscles","Body parts":"bodypart",Equipment:"equipment"},n="is-hidden";G.addEventListener("click",V);u.classList.remove(n);p.classList.add(n);async function V(e){const t=e.target.closest(".exercise-card");if(!t)return;const{filter:s,group:a}=t.dataset;await Y(K[s],a).then(Z),u.classList.add(n),p.classList.remove(n),J.addEventListener("click",i);function i(r){console.log(r.target),currentTarget===r.target&&(u.classList.remove(n),p.classList.add(n))}}async function Y(e,t){return fetch(`${z}exercises?${e}=${t}&page=1&limit=12`).then(s=>s.json()).then(s=>s.results)}function X({bodyPart:e,target:t,name:s,burnedCalories:a,rating:i,time:r,_id:d}){return`<li class = "list-exercises" id="${d}" data-filter="${s}"><div class="options">
+  `}function Q(e){var s;const t=(s=e.target.closest(".page"))==null?void 0:s.dataset.page;t&&m(g,t).then(v)}document.querySelector(".exercises-nav-list");const u=document.querySelector(".exercises"),P=document.querySelector(".exercises-container");document.querySelector(".exercise-card");const j=document.querySelector(".exercises-gallery");document.querySelector(".header-box");const F=document.querySelector(".exercises-search"),p=document.querySelector(".exercise-filters-section"),O="https://energyflow.b.goit.study/api/",N={Muscles:"muscles","Body parts":"bodypart",Equipment:"equipment"},c="is-hidden";P.addEventListener("click",G);u.classList.remove(c);p.classList.add(c);async function G(e){const t=e.target.closest(".exercise-card");if(!t)return;const{filter:s,group:a}=t.dataset;await W(N[s],a).then(z),u.classList.add(c),p.classList.remove(c),F.addEventListener("click",i);function i(r){console.log(r.target),currentTarget===r.target&&(u.classList.remove(c),p.classList.add(c))}}async function W(e,t){return fetch(`${O}exercises?${e}=${t}&page=1&limit=12`).then(s=>s.json()).then(s=>s.results)}function J({bodyPart:e,target:t,name:s,burnedCalories:a,rating:i,time:r,_id:d}){return`<li class = "list-exercises" id="${d}" data-filter="${s}"><div class="options">
   <div class="box-up">
   <div class="box-left">
   <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
@@ -33,7 +33,7 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./
             <p class="options-item"><span class="hid-txt">Body part:</span>${e}</p>
             <p class="options-item"><span class="hid-txt">Target:</span>${t}</p></div>
             </div>
-            </li>`}function Z(e){W.innerHTML=e.map(X).join("")}const S=document.querySelector(".modal-backdrop"),ee=document.querySelector(".exercises-gallery");document.querySelector(".exercise-close-btn");const te="is-hidden";ee.addEventListener("click",se);async function se(e){if(e.target.nodeName!=="BUTTON")return;const t=e.target.closest("[data-filter]").dataset.filter;(await l.get("https://energyflow.b.goit.study/api/exercises").then(a=>a.data.results).catch(a=>console.log(a))).map(a=>{a.name===t&&(S.classList.remove(te),ae(a))})}function ae(e={}){const{_id:t,bodyPart:s,equipment:a,time:i,target:r,burnedCalories:d,gifUrl:L,name:w,popularity:q,rating:E,description:M}=e,k=`<div class="exercises-modal-window">
+            </li>`}function z(e){j.innerHTML=e.map(J).join("")}const $=document.querySelector(".modal-backdrop"),K=document.querySelector(".exercises-gallery");document.querySelector(".exercise-close-btn");const V="is-hidden";K.addEventListener("click",Y);async function Y(e){if(e.target.nodeName!=="BUTTON")return;const t=e.target.closest("[data-filter]").dataset.filter;(await l.get("https://energyflow.b.goit.study/api/exercises").then(a=>a.data.results).catch(a=>console.log(a))).map(a=>{a.name===t&&($.classList.remove(V),X(a))})}function X(e={}){const{_id:t,bodyPart:s,equipment:a,time:i,target:r,burnedCalories:d,gifUrl:S,name:w,popularity:L,rating:q,description:E}=e,T=`<div class="exercises-modal-window">
           <div class="exercise-container">
             <button class="exercise-close-btn" type="button">
               <svg
@@ -47,7 +47,7 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./
             </button>
             <div class="exercise-image-wrapper">
               <img
-                src="${L}"
+                src="${S}"
                 alt=""
                 class="exercise-image-modal"
               />
@@ -55,7 +55,7 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./
             <div class="exercise-info-wrapper">
               <div class="exercise-name-container decorate-line">
                 <p class="exercise-name">${w}</p>
-                <p class="exercise-rating">${E}</p>
+                <p class="exercise-rating">${q}</p>
               </div>
               <div class="exercise-params-container decorate-line">
                 <ul class="exercise-params-list">
@@ -73,14 +73,14 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./
                   </li>
                   <li class="exercise-params-card">
                     <p class="exercise-param-name">Popular</p>
-                    <p class="exercise-param-value exercise-param-popularuty">${q}</p>
+                    <p class="exercise-param-value exercise-param-popularuty">${L}</p>
                   </li>
                   <li class="exercise-params-card">
                   <p class="exercise-param-name">Burned calories</p>
                   <p class="exercise-param-value exercise-param-popularuty">${d}/${i} m</p></li>
                 </ul>
               </div>
-              <p class="exercise-descr">${M}</p>
+              <p class="exercise-descr">${E}</p>
               <div class="exercise-buttons">
                 <button
                   class="exercise-favorite-btn"
@@ -101,5 +101,5 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as l,i as c}from"./
               </div>
             </div>
           </div>
-        </div>`;S.innerHTML=k}document.getElementById("footerForm").addEventListener("submit",function(e){e.preventDefault();const t=document.getElementById("emailInput").value;if(!ie(t)){c.error({title:"Error",message:"Please enter a valid email address",position:"topRight"});return}l.post("https://energyflow.b.goit.study/api/subscription",{email:t},{headers:{"Content-Type":"application/json"}}).then(function(s){console.log(s.data),c.success({title:"Success",message:"Subscription created successfully!",position:"topRight"}),document.getElementById("emailInput").value=""}).catch(function(s){console.error("Error:",s),c.error({title:"Error",message:"Failed to create subscription",position:"topRight"})})});function ie(e){return/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)}
+        </div>`;$.innerHTML=T}document.getElementById("footerForm").addEventListener("submit",function(e){e.preventDefault();const t=document.getElementById("emailInput").value;if(!Z(t)){n.error({title:"Error",message:"Please enter a valid email address",position:"topRight"});return}l.post("https://energyflow.b.goit.study/api/subscription",{email:t},{headers:{"Content-Type":"application/json"}}).then(function(s){console.log(s.data),n.success({title:"Success",message:"Subscription created successfully!",position:"topRight"}),document.getElementById("emailInput").value=""}).catch(function(s){console.error("Error:",s),n.error({title:"Error",message:"Failed to create subscription",position:"topRight"})})});function Z(e){return/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)}
 //# sourceMappingURL=commonHelpers2.js.map
