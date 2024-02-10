de1f9fb02380884633bdd164bf309d7178fa8d43
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-10 10:00:55 +0000

    Deploying to gh-pages from @ Liubko89/project-team-6@09ecef0bb3a23124d4a752c842e09c84b2ae7a03 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as y}from"./assets/vendor-0cb09735.js";y.defaults.baseURL="https://energyflow.b.goit.study/api";const n=document.querySelector(".exercises-nav-list"),$=document.querySelector(".exercises-container"),w="https://energyflow.b.goit.study/api";n.addEventListener("click",E);l("Muscles").then(o);async function E(e){const{filter:s}=e.target.dataset;if(!s)return;await l(s).then(o);const a=n.querySelector(".active"),i=e.target;a.disabled=!1,i.disabled=!0,a.classList.remove("active"),i.classList.add("active")}async function l(e){return fetch(`${w}/filters?filter=${e}&page=1&limit=12`).then(s=>s.json()).then(s=>s.results)}function o(e){$.innerHTML=e.map(L).join("")}function L({name:e,filter:s,imgUrl:a}){return`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as w}from"./assets/vendor-0cb09735.js";w.defaults.baseURL="https://energyflow.b.goit.study/api";const p=document.querySelector(".exercises-nav-list"),E=document.querySelector(".exercises-container"),c=document.querySelector(".pagination"),L="https://energyflow.b.goit.study/api";let n="Muscles";p.addEventListener("click",S);c.addEventListener("click",M);l(n).then(o);async function S(e){const{filter:s}=e.target.dataset;if(!s)return;n=s,await l(s).then(o);const a=p.querySelector(".active"),t=e.target;a.disabled=!1,t.disabled=!0,a.classList.remove("active"),t.classList.add("active")}async function l(e,s=1){return fetch(`${L}/filters?filter=${e}&page=${s}&limit=12`).then(a=>a.json())}function o({results:e,page:s,totalPages:a}){q(e),k(a,s)}function q(e){E.innerHTML=e.map(C).join("")}function C({name:e,filter:s,imgUrl:a}){return`
     <li class="exercise-card" data-filter="${e}">
       <div class="exercise-card-img">
         <img class="exercise-card-img" src="${a}" alt="${e}">
@@ -8,11 +8,15 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as y}from"./assets/
         </div>      
       </div>
     </li>
-  `}document.querySelector(".exercises-nav-list");const c=document.querySelector(".exercises-container");document.querySelector(".exercise-card");const S=document.querySelector(".exercises-gallery"),q="https://energyflow.b.goit.study/api/",d="is-hidden";c.addEventListener("click",C);c.classList.remove(d);p("Muscles").then(u);async function C(e){console.log(e.target);const{filter:s}=e.target.dataset;s&&(await p(s).then(u),c.classList.add(d))}async function p(e){return fetch(`${q}exercises?filter=${e}&page=1&limit=12`).then(s=>s.json()).then(s=>s.results)}function k({bodyPart:e,target:s,name:a,burnedCalories:i,rating:t,time:r}){return`<li class = "list-exercises" data-filter="${a}"><div class="options">
+  `}function k(e,s){c.innerHTML=Array(e).fill(1).map((t,i)=>t+i).map(B).join("");const a=c.children[s-1];a.classList.add("active"),a.firstElementChild.disabled=!0}function B(e){return`
+    <li class="page" data-page="${e}">
+      <button>${e}</button>
+    </li>
+  `}function M(e){var a;const s=(a=e.target.closest(".page"))==null?void 0:a.dataset.page;s&&l(n,s).then(o)}document.querySelector(".exercises-nav-list");const d=document.querySelector(".exercises-container");document.querySelector(".exercise-card");const T=document.querySelector(".exercises-gallery"),j="https://energyflow.b.goit.study/api/",u="is-hidden";d.addEventListener("click",P);d.classList.remove(u);m("Muscles").then(v);async function P(e){console.log(e.target);const{filter:s}=e.target.dataset;s&&(await m(s).then(v),d.classList.add(u))}async function m(e){return fetch(`${j}exercises?filter=${e}&page=1&limit=12`).then(s=>s.json()).then(s=>s.results)}function A({bodyPart:e,target:s,name:a,burnedCalories:t,rating:i,time:r}){return`<li class = "list-exercises" data-filter="${a}"><div class="options">
   <div class="box-up">
   <div class="box-left">
   <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
-  <div class="rating-stars"><p class="rating-par">${t}</p><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
+  <div class="rating-stars"><p class="rating-par">${i}</p><svg class="icon-star" width="18" height="18"><use href="../svg/icons.svg#icon-star"></use></svg></div></div>
 
   
   <button type = "button" class="btn-start-arrow">START<svg class="icon-arrow" width="14" height="14"><use href="../svg/icons.svg#icon-arrow"></use></svg></button>
@@ -23,12 +27,12 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as y}from"./assets/
             <p class="ex-name">${a}</p>
             </div>
            
-            <p class="options-item"><span class="hid-txt">Burned calories:</span>${i}/${r}</p>
+            <p class="options-item"><span class="hid-txt">Burned calories:</span>${t}/${r}</p>
             <div class="info-ex">
             <p class="options-item"><span class="hid-txt">Body part:</span>${e}</p>
             <p class="options-item"><span class="hid-txt">Target:</span>${s}</p></div>
             </div>
-            </li>`}function u(e){S.innerHTML=e.map(k).join("")}const B=document.querySelector(".start-btn"),M=document.querySelector(".modal-backdrop");B.addEventListener("click",T);function T(e){e.preventDefault(),j().then(R).catch()}function j(){return fetch("https://energyflow.b.goit.study/api/exercises/64f389465ae26083f39b17a2").then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}function R(e={}){const{_id:s,bodyPart:a,equipment:i,time:t,target:r,burnedCalories:v,gifUrl:m,name:x,popularity:g,rating:h,description:f}=e,b=`<div class="exercises-modal-window">
+            </li>`}function v(e){T.innerHTML=e.map(A).join("")}const H=document.querySelector(".start-btn"),R=document.querySelector(".modal-backdrop");H.addEventListener("click",U);function U(e){e.preventDefault(),_().then(I).catch()}function _(){return fetch("https://energyflow.b.goit.study/api/exercises/64f389465ae26083f39b17a2").then(e=>{if(!e.ok)throw new Error(e.statusText);return e.json()})}function I(e={}){const{_id:s,bodyPart:a,equipment:t,time:i,target:r,burnedCalories:g,gifUrl:x,name:f,popularity:h,rating:b,description:y}=e,$=`<div class="exercises-modal-window">
           <div class="exercise-container">
             <button class="exercise-close-btn" type="button">
               <svg
@@ -42,15 +46,15 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as y}from"./assets/
             </button>
             <div class="exercise-image-wrapper">
               <img
-                src="${m}"
+                src="${x}"
                 alt=""
                 class="exercise-image-modal"
               />
             </div>
             <div class="exercise-info-wrapper">
               <div class="exercise-name-container">
-                <p class="exercise-name">${x}</p>
-                <p class="exercise-rating">${h}</p>
+                <p class="exercise-name">${f}</p>
+                <p class="exercise-rating">${b}</p>
               </div>
               <div class="exercise-params-container">
                 <ul class="exercise-params-list">
@@ -64,19 +68,19 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as y}from"./assets/
                   </li>
                   <li class="exercise-params-card">
                     <p class="exercise-param-name">Equipment</p>
-                    <p class="exercise-param-value exercise-param-equipment">${i}</p>
+                    <p class="exercise-param-value exercise-param-equipment">${t}</p>
                   </li>
                   <li class="exercise-params-card">
                     <p class="exercise-param-name">Popular</p>
-                    <p class="exercise-param-value exercise-param-popularuty">${g}</p>
+                    <p class="exercise-param-value exercise-param-popularuty">${h}</p>
                   </li>
                   <li class="exercise-params-card">
                   <p class="exercise-param-name">Burned calories</p>
-                  <p class="exercise-param-value exercise-param-popularuty">${v}/${t} m</p></li>
+                  <p class="exercise-param-value exercise-param-popularuty">${g}/${i} m</p></li>
                 </ul>
                 
               </div>
-              <p class="exercise-descr">${f}</p>
+              <p class="exercise-descr">${y}</p>
               <div class="exercise-buttons">
                 <button
                   class="exercise-favorite-btn"
@@ -97,5 +101,5 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";import{a as y}from"./assets/
               </div>
             </div>
           </div>
-        </div>`;M.innerHTML=b}
+        </div>`;R.innerHTML=$}
 //# sourceMappingURL=commonHelpers2.js.map
