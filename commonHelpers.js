8f67e35a6989f1eeb183624f0b6954d48adf3ced
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-11 13:15:28 +0000

    Deploying to gh-pages from @ Liubko89/project-team-6@685c8ef8382e7faf01389e901f011aaca0ffe368 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/header-hero-032bb4c8.js";const e=document.getElementById("home"),t=document.getElementById("favorites");function s(){e.classList.remove("is-active"),e.classList.add("no-active"),t.classList.remove("no-active"),t.classList.add("is-active")}s();
+import"./assets/header-hero-6378bcc3.js";const e=document.getElementById("home"),t=document.getElementById("favorites");function s(){e.classList.remove("is-active"),e.classList.add("no-active"),t.classList.remove("no-active"),t.classList.add("is-active")}s();
 //# sourceMappingURL=commonHelpers.js.map
