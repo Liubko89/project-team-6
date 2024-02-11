297d9b6d2d006c8236c19727c85fb25106edf52d
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-02-11 14:35:23 +0100

    all-favorite-without-modal

--- src/favorites.js
+++ src/favorites.js
@@ -1,17 +1,18 @@
 const home = document.getElementById('home')
 const favorites = document.getElementById('favorites')
-
-
+const back = document.querySelector('.back-image')
+const quote = document.querySelector('.quote')
+const favorites_m = document.querySelector('favorites-m')
+const home_m = document.querySelector('home-m')
 
 function onClickChange () {
     home.classList.remove('is-active')
-
     home.classList.add('no-active')
-
     favorites.classList.remove('no-active')
-
-
     favorites.classList.add('is-active')
+    quote.classList.remove('container')
+
+    back.style.display = "none"
 }
 
 onClickChange()
