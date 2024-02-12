5fe87da82c6cec9a59237884a52aa56ef891e334
olhafilipenko160695 <olyaao95@gmail.com>
2024-02-12 23:19:31 +0100

    arrow_up

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ axios.defaults.baseURL = 'https://energyflow.b.goit.study/api';
 
 let scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
-// Додавання обробника подій для прокрутки
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  // Показ або приховування кнопки при прокручуванні
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopBtn.style.display = 'block';
   } else {
@@ -18,11 +16,9 @@ function scrollFunction() {
   }
 }
 
-// Обробник події для кліку на кнопці
 scrollToTopBtn.addEventListener('click', scrollToTop);
 
-// Функція для прокрутки наверх
 function scrollToTop() {
-  document.body.scrollTop = 0; // Для Safari
-  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE та Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
