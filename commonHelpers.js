90ed43f2b673d0b725edd329c1292533c9e07d5c
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-13 22:40:06 +0000

    Deploying to gh-pages from @ Liubko89/project-team-6@eec3a5b250c4aca9663d6e2f830a577b6004b4b5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.js";const a=document.getElementById("home"),n=document.getElementById("favorites"),m=document.querySelector(".back-image"),p=document.querySelector(".quote"),s=document.querySelectorAll(".mob-nav-link"),c=document.querySelector(".content-no-favorites"),h=document.querySelector(".gallery");m.classList.add("is-hidden");function g(){a.classList.remove("is-active"),a.classList.add("no-active"),n.classList.remove("no-active"),n.classList.add("is-active"),p.classList.remove("container"),s[0].classList.remove("is-active"),s[0].classList.add("no-active"),s[1].classList.remove("no-active"),s[1].classList.add("is-active")}g();const i=JSON.parse(localStorage.getItem("favorites"));b(i);function b(t){t.length!==0?c.classList.add("is-hidden"):c.classList.remove("is-hidden")}function f(t){return t.map(({bodyPart:r,name:o,_id:l,target:d,burnedCalories:v,time:u})=>`<li class = "list-exercises" id="${l}" data-filter="${o}"><div class="options">
+import{i as n}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.js";const r=document.getElementById("home"),l=document.getElementById("favorites"),g=document.querySelector(".back-image"),f=document.querySelector(".quote"),i=document.querySelectorAll(".mob-nav-link"),d=document.querySelector(".content-no-favorites"),c=document.querySelector(".gallery");g.classList.add("is-hidden");function b(){r.classList.remove("is-active"),r.classList.add("no-active"),l.classList.remove("no-active"),l.classList.add("is-active"),f.classList.remove("container"),i[0].classList.remove("is-active"),i[0].classList.add("no-active"),i[1].classList.remove("no-active"),i[1].classList.add("is-active")}b();const t=JSON.parse(localStorage.getItem("favorites"));v(t);function v(s){s.length!==0?d.classList.add("is-hidden"):d.classList.remove("is-hidden")}function u(s){return s.map(({bodyPart:o,name:e,_id:a,target:m,burnedCalories:p,time:h})=>`<li class = "list-exercises" id="${a}" data-filter="${e}"><div class="options">
     <div class="box-up">
     <div class="box-left">
     <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
     
-    <button class="trash-btn" type="button">
-    <svg class="icon-trash" width="18" height="18"><use href="${e}#icon-trash"></use></svg>
+    <button class="trash-btn" id="trash" type="button">
+    <svg class="icon-trash" width="18" height="18"><use href="${n}#icon-trash"></use></svg>
     </button>
     
     </div>
   
     
-    <button type = "button" class="btn-start-arrow">Start<svg class="icon-arrow" width="14" height="14"><use href="${e}#icon-arrow"></use></svg></button>
+    <button type = "button" class="btn-start-arrow" id="open">Start<svg class="icon-arrow" width="14" height="14"><use href="${n}#icon-arrow"></use></svg></button>
     </div>
              
               <div class="exercises-par"> 
-              <div class="options-item-span"><svg class="icon-men" width="18" height="18"><use href="${e}#icon-running-man"></use></svg></div>
-              <h4 class="ex-name">${o}</h4>
+              <div class="options-item-span"><svg class="icon-men" width="18" height="18"><use href="${n}#icon-running-man"></use></svg></div>
+              <h4 class="ex-name">${e}</h4>
               </div>
               <div class="options-item-wrap">
-                <p class="options-item"><span class="hid-txt">Burned calories:</span>${v}/${u}</p>
-                <p class="options-item"><span class="hid-txt">Body part:</span>${r}</p>
-                <p class="options-item"><span class="hid-txt">Target:</span>${d}</p></div>
+                <p class="options-item"><span class="hid-txt">Burned calories:</span>${p}/${h}</p>
+                <p class="options-item"><span class="hid-txt">Body part:</span>${o}</p>
+                <p class="options-item"><span class="hid-txt">Target:</span>${m}</p></div>
               </div>
-             </li>`).join("")}h.innerHTML=f(i);console.log(i);
+             </li>`).join("")}c.innerHTML=u(t);c.addEventListener("click",L);function L(s){console.log(s.target.closest("li").id),console.dir(s.target.closest("button").id),s.target.closest("button").id==="trash"&&(t.find((o,e,a)=>o._id===s.target.closest("li").id?a.splice(e,1):null),localStorage.setItem("favorites",JSON.stringify(t)),c.innerHTML=u(t),v(t))}
 //# sourceMappingURL=commonHelpers.js.map
