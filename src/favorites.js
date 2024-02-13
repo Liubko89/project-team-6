0d8dc47e8d185de1caad2fdb1bc610b565caa2b7
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-13 23:25:04 +0000

    added modal to favorites

--- src/favorites.js
+++ src/favorites.js
@@ -6,6 +6,8 @@ const quote = document.querySelector('.quote');
 const text = document.querySelectorAll('.mob-nav-link');
 const toDelete = document.querySelector('.content-no-favorites');
 const gallery = document.querySelector('.gallery');
+const favoritesBackdrop = document.querySelector('.favorites-backdrop');
+const bodyOnScr = document.querySelector('body');
 
 back.classList.add('is-hidden');
 
@@ -78,9 +80,6 @@ gallery.innerHTML = renderContent(render);
 gallery.addEventListener('click', handleRemove);
 
 function handleRemove(event) {
-  console.log(event.target.closest('li').id);
-  console.dir(event.target.closest('button').id);
-
   if (event.target.closest('button').id === 'trash') {
     render.find((el, idx, arr) => {
       return el._id === event.target.closest('li').id
@@ -91,4 +90,105 @@ function handleRemove(event) {
     gallery.innerHTML = renderContent(render);
     checkContent(render);
   }
+
+  if (event.target.closest('button').id === 'open') {
+    favoritesBackdrop.classList.add('is-open');
+    bodyOnScr.classList.add('on-scroll');
+    const found = render.find(el => {
+      return el._id === event.target.closest('li').id;
+    });
+
+    const {
+      bodyPart,
+      equipment,
+      time,
+      target,
+      burnedCalories,
+      gifUrl,
+      name,
+      popularity,
+      rating,
+      description,
+    } = found;
+
+    favoritesBackdrop.innerHTML = `<div class="exercises-modal-window">
+          <div class="exercise-container">
+            <button class="exercise-close-btn" id="modal-close-btn" type="button">
+              <svg
+                class="exercise-close-icon"
+                width="10"
+                height="10"
+                aria-label="Close icon"
+              >
+                <use href="${icons}#icon-close-btn"></use>
+              </svg>
+            </button>
+            <div class="exercise-image-wrapper">
+              <img
+                src="${gifUrl}"
+                alt=""
+                class="exercise-image-modal"
+              />
+            </div>
+            <div class="exercise-info-wrapper">
+              <div class="exercise-name-container decorate-line">
+                <p class="exercise-name">${name}</p>
+                <div class="rating-container">
+                <p class="exercise-rating">${rating}</p>
+                <div class="stars-wrap">
+                  <svg class="stars-wrap-svg" width="18" height="18">
+                    <use href="${icons}#icon-star"></use>
+                  </svg>
+                  <svg class="stars-wrap-svg" width="18" height="18">
+                    <use href="${icons}#icon-star"></use>
+                  </svg>
+                  <svg class="stars-wrap-svg" width="18" height="18">
+                    <use href="${icons}#icon-star"></use>
+                  </svg>
+                  <svg class="stars-wrap-svg" width="18" height="18">
+                    <use href="${icons}#icon-star"></use>
+                  </svg>
+                  <svg class="stars-wrap-svg" width="18" height="18">
+                    <use href="${icons}#icon-star"></use>
+                  </svg>
+               </div>
+                </div>
+              </div>
+              <div class="exercise-params-container decorate-line">
+                <ul class="exercise-params-list">
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Target</p>
+                    <p class="exercise-param-value exercise-param-target">${target}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Body part</p>
+                    <p class="exercise-param-value exercise-param-bodypart">${bodyPart}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Equipment</p>
+                    <p class="exercise-param-value exercise-param-equipment">${equipment}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Popular</p>
+                    <p class="exercise-param-value exercise-param-popularuty">${popularity}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                  <p class="exercise-param-name">Burned calories</p>
+                  <p class="exercise-param-value exercise-param-popularuty">${burnedCalories}/${time} m</p></li>
+                </ul>
+              </div>
+              <p class="exercise-descr">${description}</p>
+            </div>
+          </div>
+        </div>`;
+
+    const stars = document.querySelectorAll('.stars-wrap-svg');
+    getStars(stars, Math.round(rating));
+  }
+}
+
+function getStars(arr, rate) {
+  arr.forEach((el, id) => {
+    id < rate ? el.classList.add('yellow') : null;
+  });
 }
