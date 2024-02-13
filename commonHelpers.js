107faaf5e0a50806724f086c13c21e4e5b8bc773
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-13 23:42:16 +0000

    Deploying to gh-pages from @ Liubko89/project-team-6@ccc09ff1ad272f781b094a0587c2acfbbf5ec8a4 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.js";const u=document.getElementById("home"),v=document.getElementById("favorites"),S=document.querySelector(".back-image"),q=document.querySelector(".quote"),c=document.querySelectorAll(".mob-nav-link"),m=document.querySelector(".content-no-favorites"),d=document.querySelector(".gallery"),g=document.querySelector(".favorites-backdrop"),k=document.querySelector("body");S.classList.add("is-hidden");function B(){u.classList.remove("is-active"),u.classList.add("no-active"),v.classList.remove("no-active"),v.classList.add("is-active"),q.classList.remove("container"),c[0].classList.remove("is-active"),c[0].classList.add("no-active"),c[1].classList.remove("no-active"),c[1].classList.add("is-active")}B();const r=JSON.parse(localStorage.getItem("favorites"));h(r);function h(s){s.length!==0?m.classList.add("is-hidden"):m.classList.remove("is-hidden")}function x(s){return s.map(({bodyPart:i,name:a,_id:t,target:o,burnedCalories:n,time:l})=>`<li class = "list-exercises" id="${t}" data-filter="${a}"><div class="options">
+import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.js";const h=document.getElementById("home"),x=document.getElementById("favorites"),q=document.querySelector(".back-image"),E=document.querySelector(".quote"),o=document.querySelectorAll(".mob-nav-link"),f=document.querySelector(".content-no-favorites"),u=document.querySelector(".gallery"),r=document.querySelector(".favorites-backdrop"),v=document.querySelector("body");q.classList.add("is-hidden");function B(){h.classList.remove("is-active"),h.classList.add("no-active"),x.classList.remove("no-active"),x.classList.add("is-active"),E.classList.remove("container"),o[0].classList.remove("is-active"),o[0].classList.add("no-active"),o[1].classList.remove("no-active"),o[1].classList.add("is-active")}B();const c=JSON.parse(localStorage.getItem("favorites"));w(c);function w(s){s.length!==0?f.classList.add("is-hidden"):f.classList.remove("is-hidden")}function b(s){return s.map(({bodyPart:i,name:a,_id:t,target:n,burnedCalories:l,time:d})=>`<li class = "list-exercises" id="${t}" data-filter="${a}"><div class="options">
     <div class="box-up">
     <div class="box-left">
     <div class="work-div"><p class="options-item work-div"> WORKOUT</p></div>
@@ -18,11 +18,11 @@ import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.j
               <h4 class="ex-name">${a}</h4>
               </div>
               <div class="options-item-wrap">
-                <p class="options-item"><span class="hid-txt">Burned calories:</span>${n}/${l}</p>
+                <p class="options-item"><span class="hid-txt">Burned calories:</span>${l}/${d}</p>
                 <p class="options-item"><span class="hid-txt">Body part:</span>${i}</p>
-                <p class="options-item"><span class="hid-txt">Target:</span>${o}</p></div>
+                <p class="options-item"><span class="hid-txt">Target:</span>${n}</p></div>
               </div>
-             </li>`).join("")}d.innerHTML=x(r);d.addEventListener("click",C);function C(s){if(s.target.closest("button").id==="trash"&&(r.find((i,a,t)=>i._id===s.target.closest("li").id?t.splice(a,1):null),localStorage.setItem("favorites",JSON.stringify(r)),d.innerHTML=x(r),h(r)),s.target.closest("button").id==="open"){g.classList.add("is-open"),k.classList.add("on-scroll");const i=r.find(L=>L._id===s.target.closest("li").id),{bodyPart:a,equipment:t,time:o,target:n,burnedCalories:l,gifUrl:f,name:b,popularity:w,rating:p,description:y}=i;g.innerHTML=`<div class="exercises-modal-window">
+             </li>`).join("")}u.innerHTML=b(c);u.addEventListener("click",C);function C(s){if(s.target.closest("button").id==="trash"&&(c.find((i,a,t)=>i._id===s.target.closest("li").id?t.splice(a,1):null),localStorage.setItem("favorites",JSON.stringify(c)),u.innerHTML=b(c),w(c)),s.target.closest("button").id==="open"){r.classList.add("is-open"),v.classList.add("on-scroll");const i=c.find(p=>p._id===s.target.closest("li").id),{bodyPart:a,equipment:t,time:n,target:l,burnedCalories:d,gifUrl:y,name:L,popularity:$,rating:g,description:k}=i;r.innerHTML=`<div class="exercises-modal-window">
           <div class="exercise-container">
             <button class="exercise-close-btn" id="modal-close-btn" type="button">
               <svg
@@ -36,16 +36,16 @@ import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.j
             </button>
             <div class="exercise-image-wrapper">
               <img
-                src="${f}"
+                src="${y}"
                 alt=""
                 class="exercise-image-modal"
               />
             </div>
             <div class="exercise-info-wrapper">
               <div class="exercise-name-container decorate-line">
-                <p class="exercise-name">${b}</p>
+                <p class="exercise-name">${L}</p>
                 <div class="rating-container">
-                <p class="exercise-rating">${p}</p>
+                <p class="exercise-rating">${g}</p>
                 <div class="stars-wrap">
                   <svg class="stars-wrap-svg" width="18" height="18">
                     <use href="${e}#icon-star"></use>
@@ -69,7 +69,7 @@ import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.j
                 <ul class="exercise-params-list">
                   <li class="exercise-params-card">
                     <p class="exercise-param-name">Target</p>
-                    <p class="exercise-param-value exercise-param-target">${n}</p>
+                    <p class="exercise-param-value exercise-param-target">${l}</p>
                   </li>
                   <li class="exercise-params-card">
                     <p class="exercise-param-name">Body part</p>
@@ -81,15 +81,15 @@ import{i as e}from"./assets/quote-6c451241.js";import"./assets/vendor-db25513e.j
                   </li>
                   <li class="exercise-params-card">
                     <p class="exercise-param-name">Popular</p>
-                    <p class="exercise-param-value exercise-param-popularuty">${w}</p>
+                    <p class="exercise-param-value exercise-param-popularuty">${$}</p>
                   </li>
                   <li class="exercise-params-card">
                   <p class="exercise-param-name">Burned calories</p>
-                  <p class="exercise-param-value exercise-param-popularuty">${l}/${o} m</p></li>
+                  <p class="exercise-param-value exercise-param-popularuty">${d}/${n} m</p></li>
                 </ul>
               </div>
-              <p class="exercise-descr">${y}</p>
+              <p class="exercise-descr">${k}</p>
             </div>
           </div>
-        </div>`;const $=document.querySelectorAll(".stars-wrap-svg");T($,Math.round(p))}}function T(s,i){s.forEach((a,t)=>{t<i&&a.classList.add("yellow")})}
+        </div>`;const S=document.querySelectorAll(".stars-wrap-svg");T(S,Math.round(g)),document.getElementById("modal-close-btn").addEventListener("click",()=>{r.classList.remove("is-open"),v.classList.remove("on-scroll")}),r.addEventListener("click",O),window.addEventListener("keydown",p=>{p.code==="Escape"&&m()})}}function T(s,i){s.forEach((a,t)=>{t<i&&a.classList.add("yellow")})}function m(){window.removeEventListener("keydown",I),r.classList.remove("is-open"),v.classList.remove("on-scroll")}function I(s){s.code==="Escape"&&m()}function O(s){s.currentTarget===s.target&&m()}
 //# sourceMappingURL=commonHelpers.js.map
