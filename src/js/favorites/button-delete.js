e3a14b5d50bbe5d417a711bc551dcaef389eab36
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-02-14 00:40:38 +0000

    final changes, removed spare files

--- src/js/favorites/button-delete.js
+++
@@ -1,7 +0,0 @@
-export default function getRemovButton(rating, url, _id) {
-    return `<div><button data-id="${_id}" class="favorites-button-remove" type="button">
-    <svg data-id="${_id}" class="favorites-icon-trash">
-    <use href="${url}#icon-trash-favotites"></use>
-  </svg>
-    </button></div>`;
-  }
